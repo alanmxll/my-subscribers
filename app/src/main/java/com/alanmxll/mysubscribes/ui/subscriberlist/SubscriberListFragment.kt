db7db2f4b48048de3eb6082608febbbebb5e621e
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-09-21 17:48:52 -0300

    subscribers list screen

---
+++ app/src/main/java/com/alanmxll/mysubscribes/ui/subscriberlist/SubscriberListFragment.kt
@@ -0,0 +1,9 @@
+package com.alanmxll.mysubscribes.ui.subscriberlist
+
+import androidx.fragment.app.Fragment
+import com.alanmxll.mysubscribes.R
+
+
+class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
+
+}
---
+++ app/src/main/java/com/alanmxll/mysubscribes/ui/subscriberlist/SubscriberListViewModel.kt
@@ -0,0 +1,7 @@
+package com.alanmxll.mysubscribes.ui.subscriberlist
+
+import androidx.lifecycle.ViewModel
+
+class SubscriberListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
