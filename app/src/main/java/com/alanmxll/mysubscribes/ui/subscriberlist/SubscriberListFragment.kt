311d8f944956d98c48b327b222cefbf4c67ff406
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-10-06 19:45:10 -0300

    listing DB subscribers

--- app/src/main/java/com/alanmxll/mysubscribes/ui/subscriberlist/SubscriberListFragment.kt
+++ app/src/main/java/com/alanmxll/mysubscribes/ui/subscriberlist/SubscriberListFragment.kt
@@ -3,27 +3,45 @@ package com.alanmxll.mysubscribes.ui.subscriberlist
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.alanmxll.mysubscribes.R
-import com.alanmxll.mysubscribes.data.db.entity.SubscriberEntity
+import com.alanmxll.mysubscribes.data.db.AppDatabase
+import com.alanmxll.mysubscribes.data.db.dao.SubscriberDAO
+import com.alanmxll.mysubscribes.repository.DatabaseDataSource
+import com.alanmxll.mysubscribes.repository.SubscriberRepository
 import kotlinx.android.synthetic.main.subscriber_list_fragment.*
 
 
 class SubscriberListFragment : Fragment(R.layout.subscriber_list_fragment) {
-    private lateinit var viewModel: SubscriberListViewModel
+    private val viewModel: SubscriberListViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                val subscriberDAO: SubscriberDAO =
+                    AppDatabase.getInstance(requireContext()).subscriberDAO
+
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDAO)
+
+                return SubscriberListViewModel(repository) as T
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val subscriberListAdapter = SubscriberListAdapter(
-            listOf(
-                SubscriberEntity(1, "Alan", "alanmaxwell@user.com"),
-                SubscriberEntity(2, "Laise", "laisemaia@user.com")
-            )
-        )
+        observeViewModelEvents()
+    }
 
-        recycler_subscribers.run {
-            setHasFixedSize(true)
-            adapter = subscriberListAdapter
+    private fun observeViewModelEvents() {
+        viewModel.allSubscribersEvent.observe(viewLifecycleOwner) { allSubscribers ->
+            val subscriberListAdapter = SubscriberListAdapter(allSubscribers)
+
+            with(recycler_subscribers) {
+                setHasFixedSize(true)
+                adapter = subscriberListAdapter
+            }
         }
     }
 
--- app/src/main/java/com/alanmxll/mysubscribes/ui/subscriberlist/SubscriberListViewModel.kt
+++ app/src/main/java/com/alanmxll/mysubscribes/ui/subscriberlist/SubscriberListViewModel.kt
@@ -1,7 +1,12 @@
 package com.alanmxll.mysubscribes.ui.subscriberlist
 
 import androidx.lifecycle.ViewModel
+import com.alanmxll.mysubscribes.repository.SubscriberRepository
 
-class SubscriberListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SubscriberListViewModel(
+    private val repository: SubscriberRepository
+) : ViewModel() {
+
+    val allSubscribersEvent = repository.getAllSubscribers()
+
 }
